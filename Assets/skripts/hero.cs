bf668d23c44096180e7198b4a84791d64d077103
ambience01 <wechcast6996@gmail.com>
2025-03-29 20:58:12 +0300

    init

---
+++ Assets/skripts/hero.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+
+public class hero : MonoBehaviour
+
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float jumpForce = 15f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+}
